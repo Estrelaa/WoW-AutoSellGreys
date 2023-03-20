e8ac83b9ca5b32d873f81aee5928cc57491b0895
BuildTools <kieran.winfield464@gmail.com>
2023-03-20 18:01:28 +0000

    Added selling of greys and auto repair

--- AutoSellGreys/src/AutoSellGreys.lua
+++ AutoSellGreys/src/AutoSellGreys.lua
@@ -1,3 +1,25 @@
-local function OnEvent(self, event, ...)
-    print(event,...)
+local AutoSellGreys = CreateFrame("Frame")
+
+function AutoSellGreys:AutoDoStuff(self, event, ...)
+    AutoSellGreys.RepairEquipment(self)
+    AutoSellGreys.SellGreys(self)
 end
+
+function AutoSellGreys:RepairEquipment()
+    if CanMerchantRepair() then
+        RepairAllItems()
+    end
+end
+
+function AutoSellGreys:SellGreys()
+    for bag = 0, NUM_BAG_SLOTS do
+        for slot = 1, C_Container.GetContainerNumSlots(bag) do
+            if C_Item.GetItemQualityByID(C_Container.GetContainerItemLink(bag, slot)) == 0 then
+                C_Container.UseContainerItem(bag, slot)
+            end
+        end
+    end
+end
+
+AutoSellGreys:RegisterEvent("MERCHANT_SHOW")
+AutoSellGreys:SetScript("OnEvent", AutoSellGreys.AutoDoStuff)
