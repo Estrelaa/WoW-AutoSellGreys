8a85f1dc78fab62fc2f9e3233eec0f9e2c055b2b
Estrela <kieran.winfield464@gmail.com>
2023-05-10 19:14:05 +0100

    Finally Fixed the bloody lua error on clicking a vender

--- AutoSellGreys/src/AutoSellGreys.lua
+++ AutoSellGreys/src/AutoSellGreys.lua
@@ -14,8 +14,10 @@ end
 function AutoSellGreys:SellGreys()
     for bag = BACKPACK_CONTAINER, NUM_TOTAL_EQUIPPED_BAG_SLOTS do
         for slot = 1, C_Container.GetContainerNumSlots(bag) do
-            if C_Item.GetItemQualityByID(C_Container.GetContainerItemLink(bag, slot)) == 0 then
-                C_Container.UseContainerItem(bag, slot)
+            if C_Container.GetContainerItemLink(bag, slot) ~= nil then 
+                if C_Item.GetItemQualityByID(C_Container.GetContainerItemLink(bag, slot)) == 0 then
+                    C_Container.UseContainerItem(bag, slot)
+                end
             end
         end
     end
